5fba3b2674878cca7227dbef2360bddfc130cbfa
Abhay <leader.abhay002@gmail.com>
2022-05-20 22:30:41 +0530

    readme cleared

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+    <div>
+        <Image src=""/>
+    </div>
+
+
+    </div>
+  )
+}
+
+export default Header
