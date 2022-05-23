fcf0185e99283bd3b3a065b4c0da416ea91a2116
Abhay <leader.abhay002@gmail.com>
2022-05-23 23:54:21 +0530

    logo added

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,12 +4,10 @@ import React from 'react'
 function Header() {
   return (
     <div>
-    <div>
-        <Image src=""/>
-        
-    </div>
-
-
+      <div className="relative h-10 w-20 cursor-pointer flex-shrink-0">
+        <Image layout="fill" src="/logo.png" />
+      </div>
+      <div></div>
     </div>
   )
 }
