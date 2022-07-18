b7b845b4ea1e5f2e6793bb02013d3947f5529106
Abhay <leader.abhay002@gmail.com>
2022-07-18 21:25:01 +0530

    readme change

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,8 +1,8 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+import React from 'react'
+import ContentLoader from 'react-content-loader'
 
-const MyLoader = (props) => (
-  <ContentLoader 
+const MyLoader = (props: any) => (
+  <ContentLoader
     speed={2}
     width={1000}
     height={460}
@@ -11,12 +11,11 @@ const MyLoader = (props) => (
     foregroundColor="#e9e7e7"
     {...props}
   >
-    <circle cx="39" cy="33" r="17" /> 
-    <rect x="22" y="54" rx="2" ry="2" width="401" height="17" /> 
-    <rect x="62" y="26" rx="2" ry="2" width="207" height="15" /> 
+    <circle cx="39" cy="33" r="17" />
+    <rect x="22" y="54" rx="2" ry="2" width="401" height="17" />
+    <rect x="62" y="26" rx="2" ry="2" width="207" height="15" />
     <rect x="20" y="80" rx="2" ry="2" width="594" height="324" />
   </ContentLoader>
 )
 
 export default MyLoader
-
